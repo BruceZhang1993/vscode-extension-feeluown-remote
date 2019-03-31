749c7e30db60480dd488a44606a1feff4287732a
Bruce Zhang <zttt183525594@gmail.com>
2019-03-31 11:13:26 +0800

    Remove useless assignment
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,6 @@
 import * as vscode from 'vscode';
 import { status, init } from './service';
 
-let statusBarName: vscode.StatusBarItem;
-let statusBarLrc: vscode.StatusBarItem;
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
