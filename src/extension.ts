7b32ff75fa3e23448b99fa73b81b1120e45a25e4
Bruce Zhang <zttt183525594@gmail.com>
2019-04-17 19:48:24 +0800

    Move init() into start command
    Signed-off-by: Bruce Zhang <zttt183525594@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,18 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { status, init, toggle, updateConfig, prev, next } from './service';
+import { status, toggle, updateConfig, prev, next, init } from './service';
 import { CurrentPlayingProvider } from './provider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	init();
-
 	let configWatcher = vscode.workspace.onDidChangeConfiguration(updateConfig);
 
 	let commandRegister = {
 		status: vscode.commands.registerCommand('feeluown.start', () => {
+			init();
 			status();
 		}),
 		toggle: vscode.commands.registerCommand('feeluown.toggle', () => {
